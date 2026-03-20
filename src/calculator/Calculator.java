9677994caad14e319fe560adeffa68a2b6d1127c
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-03-20 20:56:31 +0900

    while문과 if문을 사용한 계산

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -7,64 +7,48 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
 
-        System.out.println("정수를 입력하세요 : ");
-        int num = scr.nextInt();
-        System.out.println("정수를 입력하세요 : ");
-        int num1 = scr.nextInt();
+        String finish = "";
 
-//        scr.nextLine();
-//        System.out.println("사칙연산을 입력하세요(+, -, *, /) : ");
-//        String sen = scr.nextLine();
+        while (!finish.equals("exit")) {
 
-//        if(sen.equals("+")) {
-//            System.out.println(num + "+" + num1 + "=" + (num+num1));
-//        } else if(sen.equals("-")) {
-//            System.out.println(num + "-" + num1 + "=" + (num-num1));
-//        } else if(sen.equals("*")) {
-//            System.out.println(num + "x" + num1 + "=" + (num * num1));
-//        } else if(sen.equals("/")) {
-//            System.out.println(num + "/" + num1 + "=" + (num/num1));
-//        } else
-//            System.out.println(" 사칙연산을 다시 입력하세요");
+            // 양의 정수 (0포함)
+            System.out.print("정수를 입력하세요 : ");
+            int num = scr.nextInt();
+            System.out.print("정수를 입력하세요 : ");
+            int num2 = scr.nextInt();
 
+            boolean flag = true;
 
-        if(num > 0 && num1 > 0) {
-            scr.nextLine();
-            System.out.println("사칙연산을 입력하세요(+, -, *, /) : ");
-            String sen = scr.nextLine();
+            while (flag) {
+                scr.nextLine();
+                System.out.print("사칙연산을 입력하세요(+, -, *, /) : ");
+                String sen = scr.nextLine();
 
-            if(sen.equals("+")) {
-                System.out.println(num + "+" + num1 + "=" + (num+num1));
-            } else if(sen.equals("-")) {
-                System.out.println(num + "-" + num1 + "=" + (num-num1));
-            } else if(sen.equals("*")) {
-                System.out.println(num + "x" + num1 + "=" + (num * num1));
-            } else if(sen.equals("/")) {
-                System.out.println(num + "/" + num1 + "=" + (num/num1));
-            } else
-                System.out.println(" 사칙연산을 다시 입력하세요");
-        }else
-            System.out.println("0은 입력될 수 없습니다. 다시 입력하세요");
+                if (sen.equals("+")) {
+                    System.out.println(num + "+" + num2 + "=" + (num + num2));
 
-        // readme.md 서식
-//# 🍎 자바 기초 계산기 과제
-//> 스파르타 백엔드 부트캠프 4기 - 김OO
-//
-//### 1. 프로젝트 소개
-//        - 자바의 기초 문법(if, switch, Scanner)을 활용한 계산기 프로그램입니다.
-//
-//### 2. 주요 기능
-//        - [x] 사칙연산 (+, -, *, /) 기능
-//                - [x] 0으로 나눌 때의 예외 처리 (중첩 if문 활용)
-//                - [x] 사용자 반복 입력 기능
-//
-//### 3. 배운 점
-//        - `Scanner`의 버퍼 문제 해결법을 익혔습니다.
-//                - `switch`문과 `if`문을 조합하여 조건 분기를 효율적으로 처리했습니다.
+                } else if (sen.equals("-")) {
+                    System.out.println(num + "-" + num2 + "=" + (num - num2));
 
+                } else if (sen.equals("*")) {
+                    System.out.println(num + "x" + num2 + "=" + (num * num2));
 
-
-
+                } else if (sen.equals("/")) {
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                    } else
+                        System.out.println(num + "/" + num2 + "=" + (num / num2));
+                    //break;
+                } else {
+                    System.out.println("사칙연산을 잘 못 입력하셨습니다. 다시 입력하세요.");
+                    continue;
+                }
+                flag = false;
+            }
+            // 더 계산하시겠습니까 에서 exit 또는 yes만 입력되게 처리하기
+            System.out.print("더 계산하시겠습니까? : ");
+            finish = scr.nextLine();
+        }
 
 
     }
