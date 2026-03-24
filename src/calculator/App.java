f3bbd7b40d4269b92adb0bf057c0185cb4f5da90
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-03-24 17:36:50 +0900

    Calculator 클래스 생성, 연산 결과 저장하는 필드, 삭제 메서드 생성

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,94 +1,86 @@
 package calculator;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        Scanner scr = new Scanner(System.in);
-        // yes 넣어주는 이유 : String finish에 ""이거나 null인 상태에서 while문으로 가면
-        // string finish = ""의 ""와 while문의 yes는 같지 않아서 false야 하고 종료시킨다.
+        // 계산하는 객체(인스턴스화)생성
+        Calculator cal = new Calculator();
+        Scanner sc = new Scanner(System.in);
         String finish = "yes";
 
-        // finish가 YES이면 실행
-        // finish.equalsIgnoreCase("yes") 이 조건을 안넣고
-        // 그냥 true만 넣었을 때 exit를 입력해도 종료안되고 "정수를 입력하세요" 출력됨
-        // 궁금한 점
-        // yes.equalsIgnoreCase("finish") 바뀌면 뭐가 달라지는 지
         while (finish.equalsIgnoreCase("yes")) {
 
-            // 문자열을 입력했을 때 예외처리 하기
-            System.out.print("정수를 입력하세요 : ");
-            int num = scr.nextInt();
-            System.out.print("정수를 입력하세요 : ");
-            int num2 = scr.nextInt();
+            System.out.print("첫 번째 정수를 입력하세요 : ");
+            int num1 = sc.nextInt();
 
+            System.out.print("두 번째 정수를 입력하세요 : ");
+            int num2 = sc.nextInt();
 
-            // 양의 정수 (0포함)
-            if (num < 0 || num2 < 0) {
-                System.out.println("0을 포함한 양의 정수를 입력하세요");
-                // num이나 num2가 음의 정수가 입력되면 다시 0을 포함한 양의 정수를 입력하기 위해 while문 처음으로 가서 정수를 입력한다.
-                // continue : 반복문(while, for)에서만 사용 가능
+            if (num1 < 0 || num2 < 0) {
+                System.out.println("0을 포함한 양의 정수를 입력하세요!");
                 continue;
             }
 
-            // 사칙연사 계산하는 부분
-            boolean flag = true;
+            System.out.print("사칙연산을 입력하세요(+, -, *, /) : ");
+            //  charAt(0) : 첫 글자만 가져온다.
+            char sen = sc.next().charAt(0);
 
-            // 참이면 실행 거짓이면 실행x / flag가 true이기 때문에 실행
-            while (flag) {
-                scr.nextLine();
-                System.out.print("사칙연산을 입력하세요(+, -, *, /) : ");
-                String sen = scr.nextLine();
-
-                // 입력된 값이 +. - , *, /이면 계산된 값을 출력해라
-                if (sen.equals("+")) {
-                    System.out.println(num + "+" + num2 + "=" + (num + num2));
-
-                } else if (sen.equals("-")) {
-                    System.out.println(num + "-" + num2 + "=" + (num - num2));
-
-                } else if (sen.equals("*")) {
-                    System.out.println(num + "x" + num2 + "=" + (num * num2));
-
-                } else if (sen.equals("/")) {
-                    // 나눗셈 두 번째 정수가 0이면 오류가 나기 때문에 입력할 수 없다는 문구 출력
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    } else
-                        System.out.println(num + "/" + num2 + "=" + (num / num2));
-                } else {
-                    System.out.println("사칙연산을 잘못 입력하셨습니다. 다시 입력하세요.");
-                    // 사칙연산을 잘못 입력했을 때 다시 사칙연산을 입력하기 위해서 while문 처음으로 가기위해 continue입력
-                    continue;
-                }
-                // 무한 반복을 종료하기 위해 false로 while문 종료
-                flag = false;
-
-            }
-
-            // while문이 종료되면서
-            // 더 계산하시겠습니까? 에서 exit 또는 yes만 입력되게 처리하기(대문자 가능)
-            boolean flag2 = true;
-
-            while (flag2) {
-
-                System.out.print("더 계산하려면 yes(YES), 종료를 원하면 exit(EXIT)를 입력하세요 : ");
-                finish = scr.nextLine();
-
-                if (finish.equalsIgnoreCase("yes")) {
-                    //yes이면 계속 진행
+            // 사칙연산을 맞게 입력하면 클래스로 이동
+            switch (sen) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
                     break;
-                } else if (finish.equalsIgnoreCase("exit")) {
-                    // exit를 입력하면 종료
-                    System.out.println("종료하겠습니다.");
-                } else {
-                    System.out.println("잘못 입력하셨습니다. 다시 입력해주세요!!");
+                default:
+                    System.out.println("사칙연산을 잘못 입력하셨습니다. 다시 입력하세요");
+                    continue;
+            }
+            int result = cal.calculate(num1, num2, sen);
+            System.out.println("result = " + result);
+
+            System.out.println("현재 계산 기록 : " + cal.getResultList());
+
+            // 더 계산할지 질문 그만하고 싶으면 exit 계속하고 싶으면 yes(대문자 가능)
+            boolean flag3 = true;
+            while (flag3) {
+                // /n 비우기
+                sc.nextLine();
+                System.out.print("더 계산하려면 yes(YES), 종료를 원하면 exit(EXIT), 수정을 원하면 update(UPDATE)를 입력하세요 : ");
+                finish = sc.nextLine();
+
+                if(finish.equalsIgnoreCase("update")) {
+                    cal.removeList();
+                    System.out.println("수정한 계산 기록 : " + cal.getResultList());
                     continue;
                 }
-                flag2 = false;
 
-            }
-        }
-    }
-}
+                switch (finish) {
+                    case "yes":
+                        break;
+                    case "exit":
+                        System.out.println("종료하겠습니다.");
+                        break;
+                    case "update":
+                        cal.removeList();
+                        break;
+                    default:
+                        System.out.println("잘못 입력하셨습니다. 다시 입력주세요!!");
+                        continue;
+                }
+                // 반복문 종료
+                flag3 = false;
+            }//while(flag3)
+        }//while(yes)
+    }//main
+}//App
+
+
+// 질문 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현하라는데
+// 가장 먼저 저장된 데이터는 새로운 값이 들어오면 덮어지는 거 아닌가?
+// setter 활용
+//            System.out.println("현재 num의 값 : " + num1);
+//            cal.setNum(num1);
--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,4 +1,63 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
-}
+
+    // 속성
+    // result 값 저장
+    private final ArrayList<Integer> resultList = new ArrayList<>();
+
+
+    // 생성자
+    Calculator() {
+    }
+
+    // 기능
+    // 사칙연산 기능
+    public int calculate(int num1, int num2, char sen) {
+
+        int result = 0;
+
+        switch (sen) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = num1 / num2;
+                break;
+        }
+        // 결과값을 리스트에 저장을 하지만 반환은 하나의 결과만 보낸다.
+        resultList.add(result);
+        return result;
+    }//calculate
+
+    // getter 활용
+    public ArrayList<Integer> getResultList() {
+        return this.resultList;
+    }
+
+    // 삭제(첫 번째 결과값만)
+    public void removeList() {
+        // 리스트안에 비어있으면?
+        // resultList안에 있는 첫번째 값을 지운다.
+        int remove = resultList.remove(0);
+    }
+
+    // setter 활용
+    //public void setreslut(?) {
+
+    }
+
+}//class
+
