05a3a5b833904535342ba3e8aeb83ec1b094f5f2
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-03-27 20:48:42 +0900

    클래스 생성

--- src/calculator2/App.java
+++ src/calculator2/App.java
@@ -1,67 +1,17 @@
 package calculator2;
 
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
-        Scanner sc = new Scanner(System.in);
-        int num1 = 0;
-        int num2 = 0;
-        char op;
-        String command;
 
-        while (true) {
+        do {
             try {
-                System.out.print("첫 번째 숫자를 입력하세요 : ");
-                num1 = sc.nextInt();
-                if (num1 < 0) {
-                    System.out.println("음수를 입력할 수 없습니다.");
-                    continue;
-                }
-                while (true) {
-                    System.out.print("사칙연산(+ - * /)을 입력하세요 : ");
-                    op = sc.next().charAt(0);
-                    if (op == '+' || op == '-' || op == '*' || op == '/') {
-                        break;
-                    } else {
-                        System.out.println("잘못된 입력입니다. 다시 입력해주세요");
-                    }
-                }
-                while (true) {
-                    System.out.print("두 번째 숫자를 입력하세요 : ");
-                    num2 = sc.nextInt();
-                    if (num2 < 0) {
-                        System.out.println("음수를 입력할 수 없습니다.");
-                        continue;
-                    }
-                    break;
-                }
+                cal.Start();
             } catch (InputMismatchException e) {
                 System.out.println("잘못 입력하셨습니다. 숫자를 입력해주세요");
-                sc.nextLine();
-                continue;
             }
-
-            int result = cal.calculate(num1, num2, op);
-            System.out.println("결과 : " + result);
-
-            while (true) {
-                sc.nextLine();
-                System.out.print("[yes] 계속 / [exit] 종료 : ");
-                command = sc.nextLine();
-                switch (command) {
-                    case "yes":
-                        break;
-                    case "exit":
-                        return;
-                    default:
-                        System.out.println("잘못 입력하셨습니다. [yes] 계속 / [exit] 종료");
-                        continue;
-                }
-                break;
-            }
-        }
+        } while (cal.checkContinue());
     }
 }
--- src/calculator2/Calculator.java
+++ src/calculator2/Calculator.java
@@ -1,20 +1,63 @@
 package calculator2;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Calculator {
 
     // 속성
     private ArrayList<Integer> results;
+    Scanner sc;
 
 
     // 생성자
-    public Calculator() {
+    Calculator() {
         this.results = new ArrayList<>();
+        this.sc = new Scanner(System.in);
     }
 
     // 기능
-    public int calculate(int num1, int num2, char op){
+    // 입력받는 메서드
+    public void Start() {
+        while (true) {
+            char op;
+            int num1;
+            int num2;
+            System.out.print("첫 번째 숫자를 입력하세요 : ");
+            num1 = sc.nextInt();
+            if (num1 < 0) {
+                System.out.println("음수를 입력할 수 없습니다.");
+                continue;
+            }
+
+            while (true) {
+                System.out.print("사칙연산(+ - * /)을 입력하세요 : ");
+                op = sc.next().charAt(0);
+                if (op == '+' || op == '-' || op == '*' || op == '/') {
+                    break;
+                }
+                System.out.println("잘못된 입력입니다. 다시 입력해주세요");
+            }
+
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요 : ");
+                num2 = sc.nextInt();
+                if (num2 < 0) {
+                    System.out.println("음수를 입력할 수 없습니다.");
+                    continue;
+                }
+                if (op == '/' && num2 == 0) {
+                    System.out.println("나눗셈 분모는 0으로 나눌 수 없습니다.");
+                    continue;
+                }
+                break;
+            }
+            System.out.println("결과 : " + calculate(num1, num2, op));
+        }
+    }
+
+    // 사칙연산 수행
+    public int calculate(int num1, int num2, char op) {
         int result = 0;
         switch (op) {
             case '+':
@@ -33,4 +76,19 @@ public class Calculator {
         return result;
     }
 
+    // 질문 메서드
+    public boolean checkContinue() {
+        while (true) {
+            sc.nextLine();
+            System.out.print("[yes] 계속 / [exit] 종료 : ");
+            String command = sc.nextLine();
+            if ("yes".equalsIgnoreCase(command)) {
+                return true;
+            }
+            if ("exit".equalsIgnoreCase(command)) {
+                return false;
+            }
+            System.out.println("잘못 입력하셨습니다. [yes] 계속 / [exit] 종료");
+        }
+    }
 }
