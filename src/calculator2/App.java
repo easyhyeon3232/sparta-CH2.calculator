3660ba4c4b02e9ce114b7c9c35d9c8eb6b49ce03
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-03-27 14:54:04 +0900

    기본적인 연산을 수행할 수 있는 계산기

---
+++ src/calculator2/App.java
@@ -0,0 +1,82 @@
+package calculator2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = 0;
+        int num2 = 0;
+        char op;
+        String command;
+
+        while (true) {
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요 : ");
+                num1 = sc.nextInt();
+                if (num1 < 0) {
+                    System.out.println("음수를 입력할 수 없습니다.");
+                    continue;
+                }
+                while (true) {
+                    System.out.print("사칙연산(+ - * /)을 입력하세요 : ");
+                    op = sc.next().charAt(0);
+                    if (op == '+' || op == '-' || op == '*' || op == '/') {
+                        break;
+                    } else {
+                        System.out.println("잘못된 입력입니다. 다시 입력해주세요");
+                    }
+                }
+                while (true) {
+                    System.out.print("두 번째 숫자를 입력하세요 : ");
+                    num2 = sc.nextInt();
+                    if (num2 < 0) {
+                        System.out.println("음수를 입력할 수 없습니다.");
+                        continue;
+                    }
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("잘못 입력하셨습니다. 숫자를 입력해주세요");
+                sc.nextLine();
+                continue;
+            }
+
+            switch (op) {
+                case '+':
+                    System.out.println("결과 : " + num1 + " + " + num2 + " = " + (num1 + num2));
+                    break;
+                case '-':
+                    System.out.println("결과 : " + num1 + " - " + num2 + " = " + (num1 - num2));
+                    break;
+                case '*':
+                    System.out.println("결과 : " + num1 + " * " + num2 + " = " + (num1 * num2));
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈에서 분모는 0이 될 수 없습니다. 처음부터 다시 입력해주세요");
+                        continue;
+                    }
+                    System.out.println("결과 : " + num1 + " / " + num2 + "  =" + (num1 / num2));
+                    break;
+            }
+
+            while (true) {
+                sc.nextLine();
+                System.out.print("[yes] 계속 / [exit] 종료 : ");
+                command = sc.nextLine();
+                switch (command) {
+                    case "yes":
+                        break;
+                    case "exit":
+                        return;
+                    default:
+                        System.out.println("잘못 입력하셨습니다. [yes] 계속 / [exit] 종료");
+                        continue;
+                }
+                break;
+            }
+        }
+    }
+}
