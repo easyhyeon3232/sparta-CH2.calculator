32c9446fe2a5046a435174fff1bc6ab66eeb5ff4
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-03-30 19:23:01 +0900

    계산기 도전 과제 완

--- src/calculator2/App.java
+++ src/calculator2/App.java
@@ -1,15 +1,12 @@
 package calculator2;
 
-import calculator.Calculator;
-
-import java.util.Collections;
 import java.util.InputMismatchException;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        ArithmeticCalculator<Double> cal = new ArithmeticCalculator<>();
+        ArithmeticCalculator cal = new ArithmeticCalculator();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -25,7 +22,6 @@ public class App {
                 }
 
                 while (true) {
-                    // + - * /로 받아야 하냐 아니면 PLUS MINUS로 받아야 할까?
                     System.out.print("사칙연산(+ - * /)을 입력하세요 : ");
 
                     // + - * / 로 입력받을 때
@@ -33,8 +29,6 @@ public class App {
                     op = OperatorType.fromSymbol(input);
 
                     if (op != null) {
-                        System.out.println("input = " + input);
-                        System.out.println("op = " + op);
                         break;
                     }
                     System.out.println("잘못된 입력입니다. 다시 입력해주세요");
@@ -55,10 +49,10 @@ public class App {
                     }
                     double result = cal.calculate(num1, num2, op);
                     System.out.println("result = " + result);
-                    System.out.println("결과 : " + cal.getResult());
+                    System.out.println("결과 : " + cal.getResultList());
 
 
-                } catch(InputMismatchException e){
+                } catch(InputMismatchException e) {
                     System.out.println("잘못 입력하셨습니다. 처음부터 다시 입력해주세요..");
                     sc.nextLine();
                     continue;
@@ -66,15 +60,15 @@ public class App {
 
                 while (true) {
                     sc.nextLine();
-                    System.out.print("[yes] 계속 / [exit] 종료 / [remove] 삭제 : ");
+                    System.out.print("[yes] 계속 / [exit] 종료 / [remove] 삭제 / [overs] 큰 값들 조회 / [max] 가장 큰 값 조회 / [dist] 중복 요소 제거: ");
                     String command = sc.nextLine();
                     if ("remove".equalsIgnoreCase(command)) {
                         cal.removeResult();
-                        System.out.println("삭제한 계산 기록 : " + cal.getResult());
+                        System.out.println("삭제한 계산 기록 : " + cal.getResultList());
                         continue;
                     }
 
-                    if ("get".equalsIgnoreCase(command)) {
+                    if ("overs".equalsIgnoreCase(command)) {
                         System.out.print("입력한 숫자보다 큰 값들 조회 : ");
                         double input = sc.nextDouble();
                         cal.getBigValue(input);
@@ -82,14 +76,25 @@ public class App {
                         continue;
                     }
 
+                    if("max".equalsIgnoreCase(command)) {
+                        System.out.print("가장 큰 값 조회 : " + cal.getBigNumber());
+                        continue;
+                    }
+
+                    if("dist".equalsIgnoreCase(command)) {
+                        System.out.println("중복되는 요소들을 제거");
+                        System.out.println("cal.getDistinct() = " + cal.getDistinct());
+                        continue;
+                    }
+
                     if ("yes".equalsIgnoreCase(command)) {
+                        break;
                     } else if ("exit".equalsIgnoreCase(command)) {
                         return;
                     } else {
-                        System.out.println("잘못 입력하셨습니다. [yes] 계속 / [exit] 종료 / [remove] 삭제");
-                        continue;
+                        System.out.println("잘못 입력하셨습니다.");
                     }
-                    break;
+
                 }
             }
         }
--- src/calculator2/ArithmeticCalculator.java
+++ src/calculator2/ArithmeticCalculator.java
@@ -4,45 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-enum OperatorType {
-    PLUS('+'),
-    MINUS('-'),
-    MULTIPLY('*'),
-    DIVIDE('/');
-
-    // 속성
-    private final char symbol;
-
-    // 생성자
-    OperatorType(char s) {
-        this.symbol = s;
-    }
-
-    //기능
-    public static OperatorType fromSymbol(char symbol) {
-        return switch (symbol) {
-            case '+' -> PLUS;
-            case '-' -> MINUS;
-            case '*' -> MULTIPLY;
-            case '/' -> DIVIDE;
-            default -> null;
-        };
-    }
-}
-
 // <T extends Number> : T는 숫자 종류라면 무엇이든 가능하다는 제한
-public class ArithmeticCalculator<T extends Number> {
+public class ArithmeticCalculator {
 
-
-    // 속성
-    // 결과값들을 T 타입의 리스트로 저장
     private List<Double> resultsList = new ArrayList<>();
 
-    // 생성자
-
-    // 기능
     // 사칙연산 수행
-    public double calculate(T num1, T num2, OperatorType op) {
+    public <T extends Number, S extends Number> double calculate(T num1, S num2, OperatorType op) {
         // 입력을 더블로 받아서 계산하면 정수, 실수 모두 커버 가능
         double result = 0;
         double n1 = num1.doubleValue();
@@ -65,7 +33,7 @@ public class ArithmeticCalculator<T extends Number> {
         return result;
     }
 
-    public List<Double> getResult() {
+    public List<Double> getResultList() {
         return this.resultsList;
     }
 
@@ -78,6 +46,22 @@ public class ArithmeticCalculator<T extends Number> {
                 .filter(n -> n > target)
                 .collect(Collectors.toList());
 
+    }
+
+    public Double getBigNumber () {
+        return resultsList.stream()
+                // Double::campareTo :  더블 클래스에 있는 숫자 비교 기능(campareTo)을 가져다 쓰갰다.
+                // 람다식 : (a, b) -> a.campareTo(b)
+                .max(Double::compareTo)
+                // 리스트안에 비어있을 경우 예외를 던져라
+                .orElseThrow(() -> new IllegalArgumentException("데이터가 비어있어 최댓값을 구할 수 없습니다."));
+    }
+
+    // 중복되는 요소들을 제거하고 새로운 스트림 반환
+    public List<Double> getDistinct() {
+         return resultsList.stream()
+                .distinct()
+                .collect(Collectors.toList());
     }
 
 }
