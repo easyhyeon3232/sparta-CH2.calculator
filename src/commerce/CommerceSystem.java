6b57195e4e84748ac33aff48c9a6d2f08e3f7eb6
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-03-31 17:08:43 +0900

    커머스 STEP 1 끝

--- src/commerce/CommerceSystem.java
+++ src/commerce/CommerceSystem.java
@@ -2,5 +2,6 @@ package commerce;
 
 import java.util.List;
 
+// 프로그램 비즈니스 로직 클래스
 public class CommerceSystem {
 }
--- src/commerce/Product.java
+++ src/commerce/Product.java
@@ -1,26 +1,25 @@
 package commerce;
 
-
+// 개별 상품 정보를 가지는 클래스
 public class Product {
     // 속성
     private String name;
-    private String price;
+    private int price;
     private String description;
     private int stock;
 
     // 생성자
-    public Product(String name, String price, String description) {
+    public Product(String name, int price, String description) {
         this.name = name;
         this.price = price;
         this.description = description;
-        this.
     }
 
     public String getName() {
         return this.name;
     }
 
-    public String getPrice() {
+    public int getPrice() {
         return this.price;
     }
 
@@ -28,6 +27,26 @@ public class Product {
         return this.description;
     }
 
-    public void products() {
+
+    @Override
+    // 객체 출력 시 주소값 대신 상품의 상세 정보를 문자열로 반환하기 위한 메서드
+    public String toString() {
+        return name + " | " + String.format("%d", price) + " | " + description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
     }
 }
